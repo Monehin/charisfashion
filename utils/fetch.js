5040ea1229af8f520b595e757830e3c762a7351c
monehin <e.monehin@live.com>
2021-07-28 13:27:01 +0200

    optimize performace

--- utils/fetch.js
+++ utils/fetch.js
@@ -18,4 +18,6 @@ export async function postData(url = '', data = {}) {
 
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:1337' : 'http://localhost:1337';
+export const server = dev
+  ? 'http://localhost:1337'
+  : 'https://charisfashion.herokuapp.com';
